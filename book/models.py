dda4fa939a3b5793bfa51f940475c34250f2aa8b
NavidKalashi <kalashinavid@gmail.com>
2023-11-14 18:53:13 +0330

    first views and models added

--- book/models.py
+++ book/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    price = models.IntegerField(default=0, null=True, blank=True)
+    about = models.TextField(null=True, blank=True)
+    category = models.CharField(max_length=200, null=True, blank=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
--- book/views.py
+++ book/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def books(request):
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, 'book/books.html', context)
+
+def book(request, pk):
+    book = Book.objects.get(id=pk)
+    context = {'book': book}
+    return render(request, 'book/single_book.html', context)
