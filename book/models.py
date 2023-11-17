5bd3fbf97785a2197956bb97a41f163738c64f7f
NavidKalashi <kalashinavid@gmail.com>
2023-11-17 16:32:31 +0330

    add category model

--- book/models.py
+++ book/models.py
@@ -2,12 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     price = models.IntegerField(default=0, null=True, blank=True)
     about = models.TextField(null=True, blank=True)
-    category = models.CharField(max_length=200, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
--- book/urls.py
+++ book/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.books, name='books'),
+    path('', views.books, name='books'),
     path('books/<str:pk>/', views.book, name='single-book'),
 ]
